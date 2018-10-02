31fa981ac3505c2e66df8e9be51d73b8f24bc95f
shengyuan1119 <43777851+shengyuan1119@users.noreply.github.com>
2018-10-02 21:47:59 +0800

    Add files via upload

--- icon.js
+++ icon.js
@@ -1,56 +1,183 @@
-var center = [-7908084, 6177492];
-
-var googleLayer = new olgm.layer.Google();
-
-var osmLayer = new ol.layer.Tile({
-  source: new ol.source.OSM(),
-  visible: false
-});
-
-var source = new ol.source.Vector();
-var feature = new ol.Feature(new ol.geom.Point(center));
-feature.setStyle(new ol.style.Style({
-    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-      anchor: [0.5, 46],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'pixels',
-      size: [32, 48],
-      scale: 0.75,
-      src: 'data/icon.png',
-      rotation: 0.25 * Math.PI,
-      opacity: 0.5
-    }))
-  }));
-source.addFeature(feature);
-var vector = new ol.layer.Vector({
-  source: source
-});
-
-var map = new ol.Map({
-  // use OL3-Google-Maps recommended default interactions
-  interactions: olgm.interaction.defaults(),
-  layers: [
-    googleLayer,
-    osmLayer,
-    vector
-  ],
-  target: 'map',
-  view: new ol.View({
-    center: center,
-    zoom: 12
-  })
-});
-
-var olGM = new olgm.OLGoogleMaps({
-  map: map,
-  mapIconOptions: {
-    useCanvas: true
-  }}); // map is the ol.Map instance
-olGM.activate();
+var myLatLng = {lat:25.047923,lng:121.516954};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: myLatLng,});
+function initialize(){
+  var myLatLng = {lat:25.047923,lng:121.516954};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: myLatLng,});
+}
 
 
-function toggle() {
-  googleLayer.setVisible(!googleLayer.getVisible());
-  osmLayer.setVisible(!osmLayer.getVisible());
-  alert("hello");
+
+function toggle(value) {
+  //googleLayer.setVisible(!googleLayer.getVisible());
+  //osmLayer.setVisible(!osmLayer.getVisible());
+  initialize();
+const wmsMapType = new google.maps.ImageMapType({
+    maxZoom: 18,
+    minZoom: 7,
+    name: "EMAP",
+    // 每個磚格設定為 256 X 256
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+    getTileUrl: function(coord, zoom) {
+      return `http://gis.sinica.edu.tw/taipei/file-exists.php?img=Taipei_1895-png-${zoom}-${coord.x}-${coord.y}`;
+    }
+  });
+  map.overlayMapTypes.insertAt(0, wmsMapType);
 };
+
+
+
+function toggle2(value) {
+  //googleLayer.setVisible(!googleLayer.getVisible());
+  //osmLayer.setVisible(!osmLayer.getVisible());
+initialize();
+const wmsMapType = new google.maps.ImageMapType({
+    maxZoom: 18,
+    minZoom: 7,
+    name: "EMAP",
+    // 每個磚格設定為 256 X 256
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+    getTileUrl: function(coord, zoom) {
+      return `http://gis.sinica.edu.tw/taipei/file-exists.php?img=Taipei_1903-png-${zoom}-${coord.x}-${coord.y}`;
+    }
+  });
+  map.overlayMapTypes.insertAt(0, wmsMapType);
+};
+
+
+function toggle3(value) {
+  //googleLayer.setVisible(!googleLayer.getVisible());
+  //osmLayer.setVisible(!osmLayer.getVisible());
+initialize();
+const wmsMapType = new google.maps.ImageMapType({
+    maxZoom: 18,
+    minZoom: 7,
+    name: "EMAP",
+    // 每個磚格設定為 256 X 256
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+    getTileUrl: function(coord, zoom) {
+      return `http://gis.sinica.edu.tw/taipei/file-exists.php?img=Taipei_1907-png-${zoom}-${coord.x}-${coord.y}`;
+    }
+  });
+  map.overlayMapTypes.insertAt(0, wmsMapType);
+};
+
+function toggle4(value) {
+  //googleLayer.setVisible(!googleLayer.getVisible());
+  //osmLayer.setVisible(!osmLayer.getVisible());
+initialize();
+const wmsMapType = new google.maps.ImageMapType({
+    maxZoom: 18,
+    minZoom: 7,
+    name: "EMAP",
+    // 每個磚格設定為 256 X 256
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+    getTileUrl: function(coord, zoom) {
+      return `http://gis.sinica.edu.tw/taipei/file-exists.php?img=Taipei_1910-png-${zoom}-${coord.x}-${coord.y}`;
+    }
+  });
+  map.overlayMapTypes.insertAt(0, wmsMapType);
+};
+
+
+function toggle5(value) {
+  //googleLayer.setVisible(!googleLayer.getVisible());
+  //osmLayer.setVisible(!osmLayer.getVisible());
+initialize();
+const wmsMapType = new google.maps.ImageMapType({
+    maxZoom: 18,
+    minZoom: 7,
+    name: "EMAP",
+    // 每個磚格設定為 256 X 256
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+    getTileUrl: function(coord, zoom) {
+      return `http://gis.sinica.edu.tw/taipei/file-exists.php?img=Taipei_1911-png-${zoom}-${coord.x}-${coord.y}`;
+    }
+  });
+  map.overlayMapTypes.insertAt(0, wmsMapType);
+};
+
+
+function toggle6(value) {
+  //googleLayer.setVisible(!googleLayer.getVisible());
+  //osmLayer.setVisible(!osmLayer.getVisible());
+initialize();
+const wmsMapType = new google.maps.ImageMapType({
+    maxZoom: 18,
+    minZoom: 7,
+    name: "EMAP",
+    // 每個磚格設定為 256 X 256
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+    getTileUrl: function(coord, zoom) {
+      return `http://gis.sinica.edu.tw/taipei/file-exists.php?img=Taipei_1903-png-${zoom}-${coord.x}-${coord.y}`;
+    }
+  });
+  map.overlayMapTypes.insertAt(0, wmsMapType);
+};
+
+
+function toggle7(value) {
+  //googleLayer.setVisible(!googleLayer.getVisible());
+  //osmLayer.setVisible(!osmLayer.getVisible());
+initialize();
+const wmsMapType = new google.maps.ImageMapType({
+    maxZoom: 18,
+    minZoom: 7,
+    name: "EMAP",
+    // 每個磚格設定為 256 X 256
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+    getTileUrl: function(coord, zoom) {
+      return `http://gis.sinica.edu.tw/taipei/file-exists.php?img=Taipei_1914-png-${zoom}-${coord.x}-${coord.y}`;
+    }
+  });
+  map.overlayMapTypes.insertAt(0, wmsMapType);
+};
+
+
+function toggle8(value) {
+  //googleLayer.setVisible(!googleLayer.getVisible());
+  //osmLayer.setVisible(!osmLayer.getVisible());
+initialize();
+const wmsMapType = new google.maps.ImageMapType({
+    maxZoom: 18,
+    minZoom: 7,
+    name: "EMAP",
+    // 每個磚格設定為 256 X 256
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+    getTileUrl: function(coord, zoom) {
+      return `http://gis.sinica.edu.tw/taipei/file-exists.php?img=Taipei_1916-png-${zoom}-${coord.x}-${coord.y}`;
+    }
+  });
+  map.overlayMapTypes.insertAt(0, wmsMapType);
+};
+
+
+function toggle9(value) {
+  //googleLayer.setVisible(!googleLayer.getVisible());
+  //osmLayer.setVisible(!osmLayer.getVisible());
+initialize();
+const wmsMapType = new google.maps.ImageMapType({
+    maxZoom: 18,
+    minZoom: 7,
+    name: "EMAP",
+    // 每個磚格設定為 256 X 256
+    tileSize: new google.maps.Size(256, 256),
+    isPng: true,
+    getTileUrl: function(coord, zoom) {
+      return `http://gis.sinica.edu.tw/taipei/file-exists.php?img=Taipei_10K_1919-png-${zoom}-${coord.x}-${coord.y}`;
+    }
+  });
+  map.overlayMapTypes.insertAt(0, wmsMapType);
+};
+
